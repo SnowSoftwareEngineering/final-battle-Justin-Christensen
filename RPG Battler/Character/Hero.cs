8d41875315330e873b74396d770362c0fc903d28
Justin Christensen <justin.christensen4@students.snow.edu>
2024-11-16 16:51:05 -0700

    starter code

--- RPG Battler/Character/Hero.cs
+++ RPG Battler/Character/Hero.cs
@@ -21,15 +21,50 @@ namespace RPG_Battler.Character
         public List<Equipment> Equipment { get; set; }
 
         public Hero()
-        {       
+        {
+            Name = "Unknown";
+            Level = 0;
+            Health = 1; 
+            Power = 1;
+            Luck = 1;
+            Mana = 1;  
         }
 
         public void LevelUp()
         {
+            if (CombatClass == CombatClass.Wizard)
+            {
+            Random random = new Random();
+            Health += random.Next(1, 15);
+            Power += random.Next(3, 5);
+            Luck += random.Next(1, 3);
+            }
+            else if (CombatClass == CombatClass.Warrior)
+            {
+                Random random = new Random();
+                Health += random.Next(10, 20);
+                Power += random.Next(1, 3);
+                Luck += random.Next(1, 3);
+            }
+            else if (CombatClass == CombatClass.Rogue)
+            {
+                Random random = new Random();
+                Health += random.Next(1, 15);
+                Power += random.Next(1, 3);
+                Luck += random.Next(3, 5);
+            }
         }
 
         public void CalculateTotals()
         {
+        }
+
+        public void DisplayStats(bool totVsNat = true) //true for total false for natural
+        {
+            Console.WriteLine("Level: " + Level);
+            Console.WriteLine("Health: " + Health);
+            Console.WriteLine("Power: " + Power);
+            Console.WriteLine("Luck: " + Luck);
         }
     }
 }
